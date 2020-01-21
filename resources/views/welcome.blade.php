d3ebfd216cb3d9d1d60a9aadb0d020d43596252e
Andrei Ciobanu <cio3banu@gmail.com>
2020-01-21 22:32:21 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="{{app()->getLocale()}}">
+<head>
+<meta charset="utf-8">
+<title>Hello</title>
+</head>
+<body>
+<h1>Hello to Me Andrei</h1>
+<p>Laravel {{App::VERSION()}}</p>
+</body>
+</html>
